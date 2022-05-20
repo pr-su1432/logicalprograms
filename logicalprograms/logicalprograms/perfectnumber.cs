287e8c4e3653ee0c60e51474b0749cefcc093169
prasanna lakshmi <prasannalakshmikathi@gmail.com>
2022-05-20 22:32:28 +0530

    [prasanna]: perfect number or not

---
+++ logicalprograms/logicalprograms/perfectnumber.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logicalprograms
+{
+    internal class perfectnumber
+    {
+        public void number()
+        {
+            int n, i, sum;
+
+            Console.Write("\n\n");
+            Console.Write("Check whether a given number is perfect number or not:\n");
+            Console.Write("\n\n");
+            Console.Write("Input the  number : ");
+            n = Convert.ToInt32(Console.ReadLine());
+            sum = 0;
+            Console.Write("The positive divisor  : ");
+            for (i = 1; i < n; i++)
+            {
+                if (n % i == 0)
+                {
+                    sum = sum + i;
+                    Console.Write("{0}  ", i);
+                }
+            }
+            Console.Write("\nThe sum of the divisor is : {0}", sum);
+            if (sum == n)
+                Console.Write("\nSo, the number is perfect.");
+            else
+                Console.Write("\nSo, the number is not perfect.");
+            Console.Write("\n");
+        }
+    }
+}

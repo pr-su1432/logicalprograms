bc72d92e177ba686656f21446496353f67c67363
prasanna lakshmi <prasannalakshmikathi@gmail.com>
2022-05-21 09:38:02 +0530

    [prasanna]: calculating coupan numbers

--- logicalprograms/logicalprograms/Program.cs
+++ logicalprograms/logicalprograms/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-logicalprograms.perfectnumber value = new logicalprograms.perfectnumber();
-value.number();
+logicalprograms.coupannumber value = new logicalprograms.coupannumber();
+value.coupan();
--- logicalprograms/logicalprograms/coupannumber.cs
+++ logicalprograms/logicalprograms/coupannumber.cs
@@ -8,5 +8,14 @@ namespace logicalprograms
 {
     internal class coupannumber
     {
+        public void coupan()
+        {
+            string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            Random random = new Random();
+            string result = new string(Enumerable.Repeat(chars, 8).Select(s => s[random.Next(s.Length)]) .ToArray());
+
+            Console.WriteLine(result);
+
+        }
     }
 }
